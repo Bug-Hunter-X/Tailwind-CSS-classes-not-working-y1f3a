2443739c426643f47b5533aa109f6929f34dd89c
BugHunter-X <askzenai@gmail.com>
2025-01-14 23:02:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+//Example showing an error where Tailwind directives are not applied correctly.
+<div class="bg-red-500 hover:bg-blue-700">
+  <p>This text should be red, and turn blue on hover.</p>
+</div>
+```
